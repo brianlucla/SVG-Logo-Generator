38e3e11666d1a3eb96e139b44e29ccb458e4890b
brianlu18 <brianlu18@g.ucla.edu>
2023-06-21 17:08:02 -0500

    finished inquirer setup

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const shape = require('./lib/shapes.js');
 
 inquirer
   .prompt([
     {
       type:'input',
       name: 'name',
-      message:'Enter text for the logo. (Must not be more than 3 characters)'
+      message:'Enter text for the logo. (Must not be more than 3 characters)',
+      validate: async function(input){
+        if (input.length > 3){
+          throw new Error('Please enter a name under 3 characters');
+        }
+        return true;
+      }
     },
     {
       type:'input',
       name: 'textColor',
-      message:'Enter a text color'
+      message:'Enter a text color.'
     },
     {
       type:'list',
       name: 'shape',
-      message:'Select a shape for the logo',
+      message:'Select a shape for the logo.',
       choices: ['square', 'triangle', 'circle']
     },
     {
